474ab7e6a858f3f0fe31336a426934ba340315c7
evgenytokmakov <evgenytokmakov@gmail.com>
2020-08-01 19:33:46 +0300

    dr

---
+++ src/Drobnaya.java
@@ -0,0 +1,10 @@
+public class Drobnaya {
+    static float fractional(float t) {
+        int value = (int) t;
+    return (t-value);
+    }
+        public static void main(String[] args) {
+            System.out.print(fractional(98.45f));
+
+    }
+}
