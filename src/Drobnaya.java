77587646fae2f501a5e8565a14c4cb43b9f76dc4
evgenytokmakov <evgenytokmakov@gmail.com>
2020-08-01 19:48:31 +0300

    dr

--- src/Drobnaya.java
+++ src/Drobnaya.java
@@ -4,7 +4,8 @@ public class Drobnaya {
     return (t-value);
     }
         public static void main(String[] args) {
-            System.out.print(fractional(98.45f));
+            System.out.println(fractional(98.45f));
+            System.out.print("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
 
     }
 }
